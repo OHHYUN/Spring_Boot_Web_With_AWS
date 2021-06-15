e9241c88d10d02becf9999012f80952b3a794cd6
OHHYUN <omplcie@gmail.com>
2021-06-15 19:24:07 +0900

    TDD jUnit LOMBOK

---
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.jojoldu.book.springboot.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_work_test(){
+
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
